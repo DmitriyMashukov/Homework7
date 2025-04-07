2eb51934d033f3a0a2b92d36fd48a4c2dbccf6c4
Dmitriy Mashukov <mashukov1988@gmail.com>
2025-04-07 17:28:05 +0700

    added the point 1 of homework

--- Program.cs
+++ Program.cs
@@ -1,19 +1,11 @@
-﻿using System.Numerics;
+﻿using Otus.ToDoList.ConsoleBot;
+using System.Numerics;
 using System.Text;
 
 namespace MyOtusProject;
 
 class Program
 {
-    static List<string> tasksForDoing = new List<string>();
-    static int maxTaskCount;
-    static int maxTaskLength;
-
-    public static void ValidateString(string? str)
-    {
-        if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
-            throw new ArgumentException("Строка не может быть null, пустой строкой или быть пробелом");
-    }
     public static int ParseAndValidateInt(string? str, int min, int max)
     {
         if (!int.TryParse(str, out int number))
@@ -24,186 +16,26 @@ class Program
 
         return number;
     }
-    public static void AddTask()
-    {
-        if (tasksForDoing.Count >= maxTaskCount)
-            throw new TaskCountLimitException(maxTaskCount);
-
-        Console.WriteLine("Введите через запятую: название книги, имя и фамилию автора, количество страниц.");
-        string task = Console.ReadLine();
-        ValidateString(task);
-
-        if (task.Length > maxTaskLength)
-            throw new TaskLengthLimitException(task.Length, maxTaskLength);
-
-        if (tasksForDoing.Contains(task))
-            throw new DuplicateTaskException(task);
-
-        tasksForDoing.Add(task);
-        Console.WriteLine($"Книга '{task}' добавлена в список.");
-    }
-
-    public static void ShowTasksList()
-    {
-        if (tasksForDoing.Count == 0)
-        {
-            Console.WriteLine("Вы ещё не добавили книг в список.");
-        }
-        else
-        {
-            Console.WriteLine("\nСписок книг, которые хочу прочитать:");
-            for (int i = 0; i < tasksForDoing.Count; i++)
-                Console.WriteLine($"{i + 1}. {tasksForDoing[i]}");
-        }
-    }
-
-    public static void DeleteTask()
-    {
-        ShowTasksList();
-
-        if (tasksForDoing.Count == 0)
-            return;
-
-        Console.WriteLine("Введите номер книги для удаления:");
-        int numberOfTask;
-
-        bool isNumber = int.TryParse(Console.ReadLine(), out numberOfTask);
-
-        if (isNumber && numberOfTask > 0 && numberOfTask <= tasksForDoing.Count)
-        {
-            string removedTask = tasksForDoing[numberOfTask - 1];
-            tasksForDoing.RemoveAt(numberOfTask - 1);
-            Console.WriteLine($"Книга '{removedTask}' удалена из списка.");
-        }
-        else
-        {
-            Console.WriteLine("Данного номера книги нет в списке.");
-        }
-    }
-    public static void ReturnMenu()
-    {
-        Console.WriteLine("Для выхода в меню команд нажмите Enter.");
-        Console.ReadKey();
-    }
-    public static void DescriptionOfHelp()
-    {
-        var text = new StringBuilder("Краткая информация о том, как пользоваться программой:" +
-            "\n /start - Команда для начала работы с приложением. С помощью неё вы можете " +
-            "зарегистрироватся в приложении, введя своё имя." +
-            "\n /help - Команда со справочной информацией по работе с приложением." +
-            "\n /info - Предоставляет информацию о версии программы и дате её создания." +
-            "\n /echo - При вводе этой команды с аргументом (например, Hello), программа " +
-            "возвращает введенный текст (в данном примере \"Hello\"). Становится доступной только " +
-            "после ввода имени." +
-            "\n /addtask - Позволяет добавить новую книгу в список." +
-            "\n /showtasks - Отображает список всех добавленных книг." +
-            "\n /removetask - Позволяет удалять книги по номеру в списке." +
-            "\n /exit - Команда для завершения работы приложения.");
-        Console.WriteLine(text.ToString());
-    }
     static void Main(string[] args)
     {
-        bool isContinue = true; 
-        while (isContinue) 
+        try
         {
-            try
-            {
-                Console.WriteLine("Введите максимально допустимое количество задач от 1 до 100");
-                string? inputNumCount = Console.ReadLine();
-                maxTaskCount = ParseAndValidateInt(inputNumCount, 1, 100);
+            Console.WriteLine("Введите максимально допустимое количество задач от 1 до 100");
+            var maxTaskCount = ParseAndValidateInt(Console.ReadLine(), 1, 100);
 
-                Console.WriteLine("Введите максимально допустимую длину задачи от 1 до 100.");
-                string? inputNumLength = Console.ReadLine();
-                maxTaskLength = ParseAndValidateInt(inputNumLength, 1, 100);
+            Console.WriteLine("Введите максимально допустимую длину задачи от 1 до 100.");
+            var maxTaskLength = ParseAndValidateInt(Console.ReadLine(), 1, 100);
 
-                string name = "";
-
-                do
-                {
-                    Console.Clear();
-                    if (name != "")
-                        Console.WriteLine($"Приветствую, {name}! Чем могу помочь?" +
-                        "\n/start \n/help \n/info \n/echo \n/addtask \n/showtasks \n/removetask \n/exit");
-                    else
-                        Console.WriteLine($"Приветствую, пользователь! Список доступных команд:" +
-                        "\n/start \n/help \n/info \n/addtask \n/showtasks \n/removetask \n/exit");
-
-                    var input = Console.ReadLine();
-                    switch (input)
-                    {
-                        case "/start":
-                            Console.WriteLine("Пожалуйста, введите ваше имя:");
-                            name = Console.ReadLine();
-                            Console.WriteLine($"Имя {name} успешно установлено.");
-                            ReturnMenu();
-                            break;
-                        case "/help":
-                            DescriptionOfHelp();
-                            ReturnMenu();
-                            break;
-                        case "/info":
-                            Console.WriteLine("Версия программы 1.0. Дата создания: 26.02.2025");
-                            ReturnMenu();
-                            break;
-                        case "/echo":
-                            if (name == "")
-                            {
-                                Console.WriteLine("Чтобы активировать эту команду введите своё имя.");
-                                ReturnMenu();
-                                break;
-                            }
-                            Console.WriteLine("Введите аргумент:");
-                            var arg = Console.ReadLine();
-                            Console.WriteLine(arg);
-                            ReturnMenu();
-                            break;
-                        case "/addtask":
-                            AddTask();
-                            ReturnMenu();
-                            break;
-                        case "/showtasks":
-                            ShowTasksList();
-                            ReturnMenu();
-                            break;
-                        case "/removetask":
-                            DeleteTask();
-                            ReturnMenu();
-                            break;
-                        case "/exit":
-                            Console.WriteLine("Завершение работы программы.");
-                            isContinue = false;
-                            break;
-                        default:
-                            Console.WriteLine("Введена некорректная команда.");
-                            ReturnMenu();
-                            break;
-                    }
-                } while (isContinue);
-
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (TaskCountLimitException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (TaskLengthLimitException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (DuplicateTaskException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Произошла непредвиденная ошибка:\nType: {ex.GetType().Name}" +
-                    $"\nMessage: {ex.Message}\nStackTrace: {ex.StackTrace}\nInnerException: {ex.InnerException}");
-                Console.WriteLine("Нажмите Enter для продолжения");
-                Console.ReadLine();
-            }
+            var botClient = new ConsoleBotClient();
+            var handler = new UpdateHandler(maxTaskCount, maxTaskLength);
+            botClient.StartReceiving(handler);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла непредвиденная ошибка:\nType: {ex.GetType().Name}" +
+                $"\nMessage: {ex.Message}\nStackTrace: {ex.StackTrace}\nInnerException: {ex.InnerException}");
+            Console.WriteLine("Нажмите Enter для продолжения");
+            Console.ReadLine();
         }
     } 
 }
---
+++ UpdateHandler.cs
@@ -0,0 +1,192 @@
+﻿using Otus.ToDoList.ConsoleBot;
+using Otus.ToDoList.ConsoleBot.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyOtusProject
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        private List<string> _tasksForDoing = new List<string>();
+        private int _maxTaskCount;
+        private int _maxTaskLength;
+        private string _name = "";
+
+        public UpdateHandler(int maxTaskCount, int maxTaskLength)
+        {
+            _maxTaskCount = maxTaskCount;
+            _maxTaskLength = maxTaskLength;
+        }
+        private static void ValidateString(string? str)
+        {
+            if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
+                throw new ArgumentException("Строка не может быть null, пустой строкой или быть пробелом");
+        }
+        public static void DescriptionOfHelp(ITelegramBotClient botClient, Chat chat)
+        {
+            var text = new StringBuilder("Краткая информация о том, как пользоваться программой:" +
+                "\n /start - Команда для начала работы с приложением. С помощью неё вы можете " +
+                "зарегистрироватся в приложении, введя своё имя." +
+                "\n /help - Команда со справочной информацией по работе с приложением." +
+                "\n /info - Предоставляет информацию о версии программы и дате её создания." +
+                "\n /echo - При вводе этой команды с аргументом (например, Hello), программа " +
+                "возвращает введенный текст (в данном примере \"Hello\"). Становится доступной только " +
+                "после ввода имени." +
+                "\n /addtask - Позволяет добавить новую книгу в список." +
+                "\n /showtasks - Отображает список всех добавленных книг." +
+                "\n /removetask - Позволяет удалять книги по номеру в списке." +
+                "\n /exit - Команда для завершения работы приложения.");
+            botClient.SendMessage(chat, text.ToString());
+        }
+        public void AddTask(ITelegramBotClient botClient, Chat chat)
+        {
+            if (_tasksForDoing.Count >= _maxTaskCount)
+                throw new TaskCountLimitException(_maxTaskCount);
+
+            botClient.SendMessage(chat, "Введите через запятую: название книги, имя и фамилию автора, количество страниц.");
+            string task = Console.ReadLine();
+            ValidateString(task);
+
+            if (task.Length > _maxTaskLength)
+                throw new TaskLengthLimitException(task.Length, _maxTaskLength);
+
+            if (_tasksForDoing.Contains(task))
+                throw new DuplicateTaskException(task);
+
+            _tasksForDoing.Add(task);
+            botClient.SendMessage(chat, $"Книга '{task}' добавлена в список.");
+        }
+
+        private void ShowTasksList(ITelegramBotClient botClient, Chat chat)
+        {
+            if (_tasksForDoing.Count == 0)
+            {
+                botClient.SendMessage(chat, "Вы ещё не добавили книг в список.");
+            }
+            else
+            {
+                botClient.SendMessage(chat, "\nСписок книг, которые хочу прочитать:");
+                for (int i = 0; i < _tasksForDoing.Count; i++)
+                    botClient.SendMessage(chat, $"{i + 1}. {_tasksForDoing[i]}");
+            }
+        }
+        public void DeleteTask(ITelegramBotClient botClient, Chat chat)
+        {
+            ShowTasksList(botClient, chat);
+
+            if (_tasksForDoing.Count == 0)
+                return;
+
+            botClient.SendMessage(chat, "Введите номер книги для удаления:");
+            int numberOfTask;
+
+            bool isNumber = int.TryParse(Console.ReadLine(), out numberOfTask);
+
+            if (isNumber && numberOfTask > 0 && numberOfTask <= _tasksForDoing.Count)
+            {
+                string removedTask = _tasksForDoing[numberOfTask - 1];
+                _tasksForDoing.RemoveAt(numberOfTask - 1);
+                botClient.SendMessage(chat, $"Книга '{removedTask}' удалена из списка.");
+            }
+            else
+            {
+                botClient.SendMessage(chat, "Данного номера книги нет в списке.");
+            }
+        }
+
+        public void HandleUpdateAsync(ITelegramBotClient botClient, Update update)
+        {
+            try
+            {
+                var message = update.Message;
+                if (message == null) return;
+
+                var chat = message.Chat;
+                var input = message.Text;
+
+                switch (input)
+                {
+                    case "/start":
+                        botClient.SendMessage(chat, "Пожалуйста, введите ваше имя:");
+                        break;
+                    case "/help":
+                        DescriptionOfHelp(botClient, chat);
+                        break;
+                    case "/info":
+                        botClient.SendMessage(chat, "Версия программы 1.0. Дата создания: 26.02.2025");
+                        break;
+                    case "/echo":
+                        if (string.IsNullOrEmpty(_name))
+                        {
+                            botClient.SendMessage(chat, "Чтобы активировать эту команду введите своё имя.");
+                            break;
+                        }
+                        botClient.SendMessage(chat, "Введите аргумент:");
+                        var arg = Console.ReadLine();
+                        botClient.SendMessage(chat, arg);
+                        break;
+                    case "/addtask":
+                        AddTask(botClient, chat);
+                        break;
+                    case "/showtasks":
+                        ShowTasksList(botClient, chat);
+                        break;
+                    case "/removetask":
+                        DeleteTask(botClient, chat);
+                        break;
+                    case "/exit":
+                        botClient.SendMessage(chat, "Завершение работы программы.");
+                        break;
+                    default:
+                        if (string.IsNullOrEmpty(_name) && !input.StartsWith("/"))
+                        {
+                            _name = input;
+                            botClient.SendMessage(chat, $"Имя {_name} успешно установлено.");
+                        }
+                        else if (!string.IsNullOrEmpty(_name) && !input.StartsWith("/"))
+                        {
+                            botClient.SendMessage(chat, input);
+                        }
+                        else
+                        {
+                            if (string.IsNullOrEmpty(_name))
+                            {
+                                botClient.SendMessage(chat, $"Приветствую, пользователь! Список доступных команд:" +
+                                    "\n/start \n/help \n/info \n/addtask \n/showtasks \n/removetask \n/exit");
+                            }
+                            else
+                            {
+                                botClient.SendMessage(chat, $"Приветствую, {_name}! Чем могу помочь?" +
+                                    "\n/start \n/help \n/info \n/echo \n/addtask \n/showtasks \n/removetask \n/exit");
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (TaskCountLimitException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (TaskLengthLimitException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DuplicateTaskException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                botClient.SendMessage(update.Message.Chat, ex.Message);
+            }
+        }
+    }
+}
